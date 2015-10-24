10e12f52d775c8321ff5dd6d814fbe6263048e90
Jon Rutishauser <jrutishauser@gmail.com>
2015-10-24 00:14:37 -0400

    counting lines!

--- countingLines.js
+++ countingLines.js
@@ -1,3 +1,5 @@
 var fs = require('fs');
 var fileToRead = process.argv[2];
-
+var fileContent = fs.readFileSync(fileToRead, 'utf8').toString();
+var count = fileContent.split('\n').length - 1;
+console.log(count);
