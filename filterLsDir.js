4b885951cd5fc9a303f42fcce71f70134124729d
Jon Rutishauser <jrutishauser@gmail.com>
2015-10-25 01:18:21 -0400

    busy all day flag in the sand just for the commit goal!

--- filterLsDir.js
+++ filterLsDir.js
@@ -1,5 +1,7 @@
 var fs = require('fs');
 var path = process.argv[2];
 var extensionToFilter = process.argv[3];
+fs.readadir(path, function(){
+});
 console.log(path);
 console.log(extensionToFilter);
