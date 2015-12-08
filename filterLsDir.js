b5687baba113427259bd93f9679ce0e0a54d2231
Jon Rutishauser <jrutishauser@gmail.com>
2015-12-08 00:42:36 -0500

    module version of search directory for files and filter by extension from cl arguments

--- filterLsDir.js
+++ filterLsDir.js
@@ -1,13 +1,23 @@
+//needed components for this module only
 var fs = require('fs');
 var path = require('path');
+
 var filePath = process.argv[2];
+
 var extensionToFilter = process.argv[3];
 var answerArray = [];
+
+module.exports = function (filePath, extensionToFilter, callback){
 fs.readdir(filePath, function(err, files){
+		if (err){
+			return callback(err)	
+		}
 	files.forEach(function(element, index, array){
 		if(path.extname(element) === '.' + extensionToFilter){
 			answerArray.push(element);
 			console.log(element);
 		}		
 	})
+	callback(null, answerArray);
 });
+};
---
+++ filterModule.js
@@ -0,0 +1,12 @@
+//requiring my module
+var filterModule = require('./filterLsDir.js');
+//setting arguments to variables
+var filePath = process.argv[2];
+var extensionToFilter = process.argv[3];
+
+filterModule(filePath, extensionToFilter, function(err, files){
+	if (err){
+		return console.error('Bad news:', err)	
+	}
+});
+
